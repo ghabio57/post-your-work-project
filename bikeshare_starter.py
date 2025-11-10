c15cb76cce65c6929bc3428f5ee55ff9f1924609
Hans Wurst <hans.wurst@wursttheke.de>
2025-11-10 09:03:13 +0100

    Removed unnecessary blank lines in dokumentation

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -16,14 +16,10 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
     # get user input for month (all, january, february, ... , june)
-
-
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
 
     print('-'*40)
     return city, month, day
@@ -41,7 +37,6 @@ def load_data(city, month, day):
         df - Pandas DataFrame containing city data filtered by month and day
     """
 
-
     return df
 
 
@@ -52,13 +47,8 @@ def time_stats(df):
     start_time = time.time()
 
     # display the most common month
-
-
     # display the most common day of week
-
-
     # display the most common start hour
-
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -71,11 +61,7 @@ def station_stats(df):
     start_time = time.time()
 
     # display most commonly used start station
-
-
     # display most commonly used end station
-
-
     # display most frequent combination of start station and end station trip
 
 
@@ -90,10 +76,7 @@ def trip_duration_stats(df):
     start_time = time.time()
 
     # display total travel time
-
-
     # display mean travel time
-
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -106,13 +89,8 @@ def user_stats(df):
     start_time = time.time()
 
     # Display counts of user types
-
-
     # Display counts of gender
-
-
     # Display earliest, most recent, and most common year of birth
-
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
